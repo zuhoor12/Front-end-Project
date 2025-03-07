0826d1c34529e299333db7055d2376163ee0b389
Ali Zuhoot Bayat <90399612+zuhoor12@users.noreply.github.com>
2025-03-07 06:54:45 +0430

    send source code

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function Moon(){
+    document.getElementById('bg').style.background='rgb(18,18,35)';
+    document.getElementById('left').style.color='#fff';
+    document.getElementById('logo').src='images/alarado-icon-homepage-dark.svg';
+    document.getElementById('A').style.color='#fff';
+}
+
+function Sun(){
+    document.getElementById('bg').style.background='#fff';
+    document.getElementById('left').style.color='#000';document.getElementById('logo').src='images/alarado-icon-homepage.svg';
+    document.getElementById('A').style.color='#000';
+}
